499de63d8fd51493dd2b89779148d371a9bb894a
ayashawky574 <ayashawky574@gmail.com>
2025-10-29 14:22:58 +0300

    fix

--- script/script.js
+++ script/script.js
@@ -10,156 +10,143 @@ var emailInput = document.getElementById("emailInput");
 var finishPragraph = document.querySelector(".finishPragraph");
 var item = document.querySelector(".item");
 var email = document.getElementById("userEmail");
-var password = document.getElementById("userPassword")
-var passMsg =document.querySelector(".passMsg")
-var emailMsg =document.querySelector(".emailMsg");
-var welcomeText = document.querySelector(".welcomeText")
-var userName ;
+var password = document.getElementById("userPassword");
+var passMsg = document.querySelector(".passMsg");
+var emailMsg = document.querySelector(".emailMsg");
+var welcomeText = document.querySelector(".welcomeText");
+var userName;
 var users = [];
-if(localStorage.getItem("users")!=null){
-  users = JSON.parse(localStorage.getItem("users"))
+if (localStorage.getItem("users") != null) {
+  users = JSON.parse(localStorage.getItem("users"));
 }
-var user ;
+var user;
 
 function addUser() {
-  console.log(users);
-    if(
-        validationInputs["nameInput"]== true &&
-        validationInputs["emailInput"] == true &&
-        validationInputs["passInput"] == true 
-    ){      
-  if(!FindRepeatedEmail()){
-    user = {
-    name: nameInput.value,
-    email: emailInput.value,
-    pass: passInput.value,
-  };
-    users.push(user);
-  console.log(users);
-  localStorage.setItem("users", JSON.stringify(users));
-   finishPragraph.innerHTML ="success"
-        finishPragraph.style.color = "green"
-       finishPragraph.classList.remove("d-none")
-  return true
-  }else{
-    Swal.fire({
-  title: "This email is already exist!",
-  icon: "error"
-});
-  }
+  if (
+    validationInputs["nameInput"] == true &&
+    validationInputs["emailInput"] == true &&
+    validationInputs["passInput"] == true
+  ) {
+    if (!FindRepeatedEmail()) {
+      user = {
+        name: nameInput.value,
+        email: emailInput.value,
+        pass: passInput.value,
+      };
+      users.push(user);
+
+      localStorage.setItem("users", JSON.stringify(users));
+      finishPragraph.innerHTML = "success";
+      finishPragraph.style.color = "green";
+      finishPragraph.classList.remove("d-none");
+      return true;
     } else {
-      
-       finishPragraph.classList.remove("d-none")
-      return false 
+      Swal.fire({
+        title: "This email is already exist!",
+        icon: "error",
+      });
     }
-}
-
-
-function clearInput(){
-    nameInput.value=null;
-    emailInput.value=null;
-    passInput.value = null;
-}
-
- if (loginBtn) {
-  loginBtn.addEventListener("click", () => {
-  if(checkAccount()) {
-        location.href = "/Login-System/home/home.html";
-        
+  } else {
+    finishPragraph.classList.remove("d-none");
+    return false;
   }
+}
+
+function clearInput() {
+  nameInput.value = null;
+  emailInput.value = null;
+  passInput.value = null;
+}
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    if (checkAccount()) {
+      location.href = "/Login-System/home/home.html";
+      clearInput();
+    }
   });
 }
 
 if (signupBtn) {
   signupBtn.addEventListener("click", () => {
-   if(addUser()){
-     finishPragraph.innerHTML ="success"
-        finishPragraph.style.color = "green"
-    location.href = "/Login-System/";
-    clearInput()
-   }
+    if (addUser()) {
+      finishPragraph.classList.add("d-none");
+      location.href = "/Login-System/";
+      clearInput();
+    }
   });
 }
 
- if (homeBtn) {
+if (homeBtn) {
   homeBtn.addEventListener("click", () => {
-   location.href= "/Login-System/"
+    location.href = "/Login-System/";
   });
 }
 
-
-
-var validationInputs ={
-    nameInput :false,
-    emailInput :false ,
-    passInput : false
-}
+var validationInputs = {
+  nameInput: false,
+  emailInput: false,
+  passInput: false,
+};
 
 function validateSignup(e) {
   if (e.target.localName == "input") {
-    console.log(e.target);
-    
     var element = e.target;
-    console.log(element);
-    
+
     var regex = {
       nameInput: /^[a-z ]{6,20}$/,
-      emailInput: /^[a-zA-z-_0-9]{5,20}@gmail.com$/,
-      passInput:  /^[a-zA-Z0-9_]{5,20}$/,
+      emailInput: /^[a-zA-z-_0-9]{5,20}@gmail\.com$/,
+      passInput: /^[a-zA-Z0-9_]{5,20}$/,
     };
     var msg = element.parentElement.nextElementSibling;
-    var isvalid; 
-      function checkField(){
-           var text = element.value;
+    var isvalid;
+    function checkField() {
+      var text = element.value;
       isvalid = regex[element.id].test(text);
       if (isvalid) {
-        msg.classList.add("d-none"); 
-        validationInputs[element.id] =true  
+        msg.classList.add("d-none");
+        validationInputs[element.id] = true;
       } else {
         msg.classList.remove("d-none");
-        validationInputs[element.id] =false  
+        validationInputs[element.id] = false;
       }
-      }
-    element.addEventListener("input" , checkField)
-    element.addEventListener("blur" , checkField)
-    
+    }
+    element.addEventListener("input", checkField);
+    element.addEventListener("blur", checkField);
   }
 }
-if(location.pathname.includes("signup")){
-item.addEventListener("click", validateSignup);
+if (location.pathname.includes("signup")) {
+  item.addEventListener("click", validateSignup);
 }
 
-function FindRepeatedEmail(){
-    var isRepeated = users.some(
-        (user)=>{ 
-         return user.email=== emailInput.value;    
-        }
-        )
-     
-    return isRepeated
+function FindRepeatedEmail() {
+  var isRepeated = users.some((user) => {
+    return user.email === emailInput.value;
+  });
+
+  return isRepeated;
 }
-function checkAccount(){
- var userEmail ;
- userEmail= users.find((user) =>{
-     return user.email == email.value
-} )
-if (userEmail) {
- if(password.value == userEmail.pass){
- userName = userEmail.name;
- console.log(userName);
- localStorage.setItem( "userName" , userName)
-  return true
- }else{
-passMsg.classList.remove("d-none");
-emailMsg.classList.add("d-none");
- }
-}else{
-emailMsg.classList.remove("d-none")
-emailMsg.classList.remove("d-none")
-}
+function checkAccount() {
+  var userEmail;
+  userEmail = users.find((user) => {
+    return user.email == email.value;
+  });
+  if (userEmail) {
+    if (password.value == userEmail.pass) {
+      userName = userEmail.name;
+      console.log(userName);
+      localStorage.setItem("userName", userName);
+      return true;
+    } else {
+      passMsg.classList.remove("d-none");
+      emailMsg.classList.add("d-none");
+    }
+  } else {
+    emailMsg.classList.remove("d-none");
+    emailMsg.classList.remove("d-none");
+  }
 }
 
-if (location.pathname == '/Login-System/home/home.html') {
-welcomeText.innerText ="welcome "+ localStorage.getItem("userName");
-  
+if (location.pathname == "/Login-System/home/home.html") {
+  welcomeText.innerText = "welcome " + localStorage.getItem("userName");
 }
